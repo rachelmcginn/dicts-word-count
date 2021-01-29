edffa1442481d6efd29e42e3aa2db1035061dd99
Rachel <rachelsybille@gmail.com>
2021-01-29 04:51:28 +0000

    Completed wordcount.py

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,24 @@
 # put your code here.
+
+def wordcount(file_name):
+    the_file = open(file_name)
+    all_words = []
+
+    for line in the_file:
+        line = line.rstrip()
+        all_words.extend(line.split(' '))
+
+    word_count = {}
+    for word in all_words:
+        if word not in word_count:
+            word_count[word] = 1
+        else:
+            word_count[word] += 1
+    
+    for word, count in word_count.items():
+        print(f"{word} {count}")
+
+wordcount('test.txt')
+
+
+
